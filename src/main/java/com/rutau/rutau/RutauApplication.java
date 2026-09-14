a63e4c460332a460d211c7a784ba6a28400db1ed
sebasmoren13 <sebasmoren13@gmail.com>
2026-09-14 13:30:30 -0500

    Hola

---
+++ src/main/java/com/rutau/rutau/RutauApplication.java
@@ -0,0 +1,13 @@
+package com.rutau.rutau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rutau/rutau/RutauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rutau.rutau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
